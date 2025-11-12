9688cae767acaa0ef2e1b7ca6873a7f134c537f2
codedinpeace <vea5a.pranam05@gmail.com>
2025-11-12 20:46:40 +0530

    Day 5

---
+++ Day5/Cohort Beginner Problems/script.js
@@ -0,0 +1,74 @@
+// 1
+
+// const sayHello = () =>{
+//     console.log("Hey Javascript")
+// }
+
+// sayHello()
+
+// 2
+
+// const add = (a,b) =>{
+//     console.log( a + b )
+// }
+
+// add(5,10)
+
+// 3
+
+// const name = (name) =>{
+//     if(!name){
+//         name = "guest"
+//         console.log(name)
+//     }
+
+//     else{
+//         console.log(name)
+//     }
+// }
+
+// name("Pranam Shaw")
+
+
+// 4
+
+// const add = ([...num]) => {
+//     return num.reduce((sum, add) => {
+//         return sum + add
+//     })
+// }
+
+// console.log(add([1,2,23,34,4,5,6,7]))
+
+
+// 5 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let i = 0; i <= arr.length; i++){
+//     console.log(i)
+// }
+
+
+// 6
+
+// let obj = [{
+//     name:"Pranam",
+//     age:16,
+//     city:"Kolkata"
+// }]
+
+// obj.map((o)=>{
+//     console.log(o.name, o.age, o.city)  
+// })
+
+
+// 7
+
+// const timesUp = () =>{
+//     setTimeout(()=>{
+//         console.log("Time's Up")
+//     },2000)
+// }
+
+// timesUp()
